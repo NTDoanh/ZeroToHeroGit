60d87ceeb70330a52b1c3102e1de2c1bff8515fd
Nguyễn Trọng Doanh <102360639+NTDoanh@users.noreply.github.com>
2023-10-15 20:06:31 +0700

    Add subtract, switch case

--- calculator.go
+++ calculator.go
@@ -5,13 +5,27 @@ import (
 )
 
 func add(a, b int) {
-	fmt.Printf("%d + %d = %d\n", a, b, a + b)
+	fmt.Printf("%d + %d = %d\n", a, b, a+b)
 }
+func subtract(a, b int) {
+	fmt.Printf("%d - %d = %d\n", a, b, a-b)
+}
+
 func main() {
 	var a, b int
+	var c string
 	fmt.Print("Nhap so thu nhat: ")
 	fmt.Scanln(&a)
 	fmt.Print("Nhap so thu hai: ")
 	fmt.Scanln(&b)
-  add(a,b)
+	fmt.Print("Nhap phep tinh: ")
+	fmt.Scanln(&c)
+	switch c {
+	case "+":
+		add(a, b)
+	case "-":
+		subtract(a, b)
+	default:
+		fmt.Printf("Chua co phep nay hoac nhap sai")
+	}
 }
